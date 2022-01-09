d756088524195ac343255148f52428ada6dd3e1c
Pasang Sherpa <editp027@gmail.com>
2022-01-09 21:09:36 +0545

    Sass setup and login form created: pasang

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 import {useSelector, useDispatch} from 'react-redux';
 import {RootState, RootDispatchType} from "./store";
 import {NumberState, NumberAction} from "./types/NumberTypesAndInterfaces";
+import Login from "./auth/Login";
+import "./css/style.css"
 function App() {
   const dispatch: RootDispatchType = useDispatch() ;
   const state = useSelector<RootState>((state) => state.number) as NumberState;
  
   return (
-    <div className="App">
-      <div className="App_Number">
-        {state.number}
-      </div>
-      <button onClick={ ()=>{dispatch({type:"ADD_NUMBER"})}}> Increase</button>
-      
-    </div>
+    <>
+      <Login/>
+    </>
   );
 }
 
