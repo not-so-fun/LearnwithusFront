1fd85879f9b1f57a9f79243dc661ca44f0bd930a
yugal109 <yugalkhati570@gmail.com>
2022-01-20 17:25:52 +0545

    added main question and answer

--- src/Layout/MainQA.tsx
+++ src/Layout/MainQA.tsx
@@ -1,45 +1,53 @@
-import React,{FC, useEffect} from 'react';
+import React, { FC, useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import MainQAQuestion from "../components/mainQAComponent/MainQAQuestion";
 import MainQAAnswer from "../components/mainQAComponent/MainQAAnswer";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import useTokenAndId from "../components/ReusableLogicComponents/useTokenAndId";
 import { MainQuestionAnswerAction } from "../actions/MainQuestionAnswerAction";
 import { RootStateType } from "../stores";
-import {questionFeedListInterface, MainQuestionAnswerInterface} from "../reducers/MainQuestionAnswerReducer";
-const MainQA: FC<RouteComponentProps<any>> = ({match}) => {
-    const dispatch = useDispatch();
-    const { token, user_id } = useTokenAndId();
-    const { userInfo } = useSelector<RootStateType>(
-        (state) => state.userInfo
-      ) as any;
-    const {question}=  useSelector<RootStateType>(
-        (state) => state.mainQA
-      ) as MainQuestionAnswerInterface;
-      useEffect(() => {
-        dispatch(MainQuestionAnswerAction( token,match.params.id,));
-      }, [match, token,user_id]);
-    useEffect(() => {
-        document.title = "Learn with us | Main QA"
-    }, []);
+import { UserInfoInterface } from "../reducers/LoginReducer";
 
-    return (
-        <div className='MainQA'>
-            <div className="MainQA__Heading">
-                Main Question Answer
-            </div>
-            {/* <div className="MainQA__"></div> */}
-            <MainQAQuestion question={question}/>
-            <div className="MainQA__Answers">
-            <MainQAAnswer/>
-            <MainQAAnswer/>
-            <MainQAAnswer/>
-            <MainQAAnswer/>
-            <MainQAAnswer/>
-            </div>
-            
-        </div>
-    )
-}
+import {
+  questionFeedListInterface,
+  MainQuestionAnswerInterface,
+} from "../reducers/MainQuestionAnswerReducer";
+
+const MainQA: FC<RouteComponentProps<any>> = ({ match }) => {
+  const dispatch = useDispatch();
+  const { token, user_id } = useTokenAndId();
+  
+  const { userInfo } = useSelector<RootStateType>(
+    (state) => state.userInfo
+  ) as UserInfoInterface;
+
+  const { question } = useSelector<RootStateType>(
+    (state) => state.mainQA
+  ) as MainQuestionAnswerInterface;
+
+  useEffect(() => {
+    document.title = "Learn with us | Main QA";
+  }, []);
+
+  useEffect(() => {
+    dispatch(MainQuestionAnswerAction(token, match.params.id));
+  }, [match, token, user_id]);
+
+
+  return (
+    <div className="MainQA">
+      <div className="MainQA__Heading">Main Question Answer</div>
+      {/* <div className="MainQA__"></div> */}
+      <MainQAQuestion question={question} />
+      <div className="MainQA__Answers">
+        <MainQAAnswer />
+        <MainQAAnswer />
+        <MainQAAnswer />
+        <MainQAAnswer />
+        <MainQAAnswer />
+      </div>
+    </div>
+  );
+};
 
 export default MainQA;
--- src/components/mainQAComponent/MainQAQuestion.tsx
+++ src/components/mainQAComponent/MainQAQuestion.tsx
@@ -1,65 +1,61 @@
-import React,{FC, useEffect} from 'react';
+import React, { FC, useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { BsThreeDots } from "react-icons/bs";
-import { questionFeedListInterface } from '../../reducers/MainQuestionAnswerReducer';
+import { questionFeedListInterface } from "../../reducers/MainQuestionAnswerReducer";
 
 import { BsCaretDownFill, BsCaretUpFill } from "react-icons/bs";
 interface MainQAQuestionInterface {
-    question:questionFeedListInterface | null
+  question: questionFeedListInterface | null;
 }
-const MainQAQuestion: FC<MainQAQuestionInterface>  = ({question}) => {
-    if(question !==null){
-    return (
+const MainQAQuestion: FC<MainQAQuestionInterface> = ({ question }) => {
+  // if(question !==null){
+  return (
+    <>
+      {question && (
         <div className="MainQA__Question">
-            <div className="MainQA__Question__Top">
-                <div className="MainQA__Question__Top__ProfileData">
-                    <div className="MainQA__Question__Top__ProfileData__ProfilePhoto">
-                        <img src={question.image} className='MainQA__Question__Top__ProfileData__ProfilePhoto__Image'/>
-                    </div>
-                    <div className="MainQA__Question__Top__ProfileData__ProfileName">
-
-                    </div>
-
-                </div>
-                <div className="MainQA__Question__Top__QuestionData">
-                    <div className='MainQA__Question__Top__QuestionData__Views'>
-                        <div >Answered: Today</div>
-                        <div>Total Views: 50 views</div>
-                    </div>
-                    <div className='MainQA__Question__Top__QuestionData__Delete'>
-                        <BsThreeDots className='MainQA__Question__Top__QuestionData__Delete__Icon'/>
-                    </div>
-
-                </div>
+          <div className="MainQA__Question__Top">
+            <div className="MainQA__Question__Top__ProfileData">
+              <div className="MainQA__Question__Top__ProfileData__ProfilePhoto">
+                {/* <img src={question.image} className='MainQA__Question__Top__ProfileData__ProfilePhoto__Image'/> */}
+              </div>
+              <div className="MainQA__Question__Top__ProfileData__ProfileName"></div>
             </div>
-            <div className="MainQA__Question__MainQuestionData">
-                <div className="MainQA__Question__MainQuestionData__QuestionLikes">
-                    <div className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox">
-                        <div className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Upvote">
-                        <BsCaretUpFill className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Downvote__Logo"/>
-                        </div>
-                        <div className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Data">
-                            2k
-                        </div>
-                        <div className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Downvote">
-                            <BsCaretDownFill className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Downvote__Logo"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="MainQA__Question__MainQuestionData__QuestionTitleAndBody">
-                    <div className="MainQA__Question__MainQuestionData__QuestionTitleAndBody__Title">
-                            hello world
-                    </div>
-                    <div className="MainQA__Question__MainQuestionData__QuestionTitleAndBody__Body">
-                        
-                    </div>
-
-                </div>
+            <div className="MainQA__Question__Top__QuestionData">
+              <div className="MainQA__Question__Top__QuestionData__Views">
+                <div>Answered: Today</div>
+                <div>Total Views: 50 views</div>
+              </div>
+              <div className="MainQA__Question__Top__QuestionData__Delete">
+                <BsThreeDots className="MainQA__Question__Top__QuestionData__Delete__Icon" />
+              </div>
             </div>
-            
+          </div>
+          <div className="MainQA__Question__MainQuestionData">
+            <div className="MainQA__Question__MainQuestionData__QuestionLikes">
+              <div className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox">
+                <div className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Upvote">
+                  <BsCaretUpFill className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Downvote__Logo" />
+                </div>
+                <div className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Data">
+                  2k
+                </div>
+                <div className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Downvote">
+                  <BsCaretDownFill className="MainQA__Question__MainQuestionData__QuestionLikes__LikesBox__Downvote__Logo" />
+                </div>
+              </div>
+            </div>
+            <div className="MainQA__Question__MainQuestionData__QuestionTitleAndBody">
+              <div className="MainQA__Question__MainQuestionData__QuestionTitleAndBody__Title">
+                hello world
+              </div>
+              <div className="MainQA__Question__MainQuestionData__QuestionTitleAndBody__Body"></div>
+            </div>
+          </div>
         </div>
-    )
-    }
-}
+      )}
+    </>
+  );
+  // }
+};
 
-export default MainQAQuestion
+export default MainQAQuestion;
