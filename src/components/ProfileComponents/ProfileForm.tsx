9831323745df7ce1ecb1be9c9bde6a5fd94ffeb1
Pasang Sherpa <pasangsherpa12580@gmail.com>
2022-01-26 22:22:58 +0545

    Profile UI redesign : pasang

--- src/components/ProfileComponents/ProfileForm.tsx
+++ src/components/ProfileComponents/ProfileForm.tsx
@@ -13,24 +13,35 @@ const ProfileForm: FC<ProfileFormInterface> = ({profile_data}) => {
       <div className="Profile__Box__Top__Information__Name">
         <div className="Profile__Box__Top__Information__Name__firstName">
           <p>Firstname</p>
+          <div className="Profile__Box__Top__Information__Name__firstName__Box">
           <h4>{profile_data.first_name}</h4>
+          </div>
         </div>
         <div className="Profile__Box__Top__Information__Name__lastName">
           <p>Lastname</p>
-          <h4>{profile_data.last_name}</h4>
+          <div className="Profile__Box__Top__Information__Name__lastName__Box">
+            <h4>{profile_data.last_name}</h4>
+          </div>
         </div>
       </div>
       <div className="Profile__Box__Top__Information__BasicInfo">
         <p>Location</p>
+        <div className="Profile__Box__Top__Information__BasicInfo__Box">
         <h4>Pulchowk Campus, Lalitpur</h4>
+        </div>
       </div>
       <div className="Profile__Box__Top__Information__BasicInfo">
         <p>Interest</p>
+        <div className="Profile__Box__Top__Information__BasicInfo__Box">
+
         <h4>Web Development</h4>
+        </div>
       </div>
       <div className="Profile__Box__Top__Information__BasicInfo">
         <p>Expertise</p>
+        <div className="Profile__Box__Top__Information__BasicInfo__Box">
         <h4>Front and Back End</h4>
+        </div>
       </div>
     </div>
   );
