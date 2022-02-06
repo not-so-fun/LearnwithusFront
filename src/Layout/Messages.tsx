81714f0b4ba45d03317cb15e391a90729dcce5a4
yugal109 <yugalkhati570@gmail.com>
2022-02-06 22:08:06 +0545

    added fetch messages

--- src/Layout/Messages.tsx
+++ src/Layout/Messages.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState ,FC} from "react";
+import React, { useEffect, useState, FC } from "react";
 import RightSideBar from "./RightSideBar";
 import MessagesSidebar from "../components/MessagesComponent/MessagesSidebar";
 import useTokenAndId from "../components/ReusableLogicComponents/useTokenAndId";
@@ -7,22 +7,20 @@ import axios from "../axios";
 import { RouteComponentProps } from "react-router-dom";
 import { IoMdSend } from "react-icons/io";
 
-
-
-
-interface message {
+interface oneMessageInterface {
+  chat_room_id: String;
   message: String;
-  message_id: String;
+  message_id: string;
   user_id: String;
   created_at: String;
 }
 
 interface messagesInterface {
-  messages: [message] | null;
+  messages: [oneMessageInterface] | null;
 }
 
-const Messages:FC<RouteComponentProps<any>> = ({match}) => {
-  const [messages, setMessage] = useState<messagesInterface>({
+const Messages: FC<RouteComponentProps<any>> = ({ match }) => {
+  const [messagesState, setMessagesState] = useState<messagesInterface>({
     messages: null,
   });
 
@@ -31,16 +29,18 @@ const Messages:FC<RouteComponentProps<any>> = ({match}) => {
   useEffect(() => {
     axios
       .get(`/chat/messages/${match.params.messageId}`, {
-        headers: { 
-          "x-auth-token": token },
+        headers: {
+          "x-auth-token": token,
+        },
       })
       .then((response) => {
         console.log(response.data);
+        setMessagesState({ ...messagesState, messages: response.data });
       })
       .catch((error) => {
         console.log(error);
       });
-  }, [token,match]);
+  }, [token, match]);
 
   const MessageOther = () => {
     return (
@@ -80,20 +80,37 @@ const Messages:FC<RouteComponentProps<any>> = ({match}) => {
           </Link>
         </div>
         <div className="Messages__Left__Messages">
-          <div className="Messages__Left__Messages__Others">
-            <div className="Messages__Left__Messages__Others__Image">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg"
-                alt=""
-              />
+          {messagesState &&
+            messagesState.messages != null &&
+            messagesState.messages.map((message: oneMessageInterface) => (
+              <>
+              {message.user_id !== user_id ? 
+              <div >
+                My
+              </div>
+              :
+              <div key={message?.message_id}  className="Messages__Left__Messages__Others">
+              <div className="Messages__Left__Messages__Others__Image">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+
+                <MessageOther  />
+                {/* <MessageOther /> */}
+              </div>
             </div>
-            <div>
-              <MessageOther />
-              <MessageOther />
-            </div>
-          </div>
-        
-          
+              
+            }
+               
+
+
+
+
+              </>
+            ))}
         </div>
         <div className="Messages__Left__InputBox">
           <input
