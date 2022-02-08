9a579a7049786e1ff850e087c8b0122208bae6b1
Pasang Sherpa <pasangsherpa12580@gmail.com>
2022-02-08 23:44:02 +0545

    Notifications done:pasang

--- src/components/HomePageComponent/RequestNotification.tsx
+++ src/components/HomePageComponent/RequestNotification.tsx
@@ -3,7 +3,7 @@ import React,{FC} from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import DoneIcon from "@mui/icons-material/Done";
 import {ApproachNotificationInterface} from "../../reducers/NotificationReducer";
-import {AcceptApproachNotificationAction} from "../../actions/NotificationActions";
+import {AcceptApproachNotificationAction, DeleteApproachNotificationAction} from "../../actions/NotificationActions";
 import { useDispatch } from "react-redux";
 import useTokenAndId from "../ReusableLogicComponents/useTokenAndId";
 interface RequestNotification {
@@ -16,6 +16,10 @@ const RequestNotification: FC<RequestNotification> = ({not}) => {
 
   const Done = () =>{
     dispatch(AcceptApproachNotificationAction(token, not.approachnotification_id)); 
+  }
+  const Close =() =>{
+    dispatch(DeleteApproachNotificationAction(token, not.approachnotification_id)); 
+
   }
   return (
     <div className="HomePage__Right__MainBody__Notification__Box__RequestNotification__Noti">
@@ -32,7 +36,7 @@ const RequestNotification: FC<RequestNotification> = ({not}) => {
       </div>
       <div className="HomePage__Right__MainBody__Notification__Box__RequestNotification__Noti__Right">
         <div className="HomePage__Right__MainBody__Notification__Box__RequestNotification__Noti__Right__Reject">
-          <CloseIcon className="HomePage__Right__MainBody__Notification__Box__RequestNotification__Noti__Right__Reject__Icon" />
+          <CloseIcon className="HomePage__Right__MainBody__Notification__Box__RequestNotification__Noti__Right__Reject__Icon" onClick={Close}/>
         </div>
         <div className="HomePage__Right__MainBody__Notification__Box__RequestNotification__Noti__Right__Accept">
           <DoneIcon className="HomePage__Right__MainBody__Notification__Box__RequestNotification__Noti__Right__Accept__Icon" onClick={Done}/>
