cb2e867f5673ada55e8c1d25adeb4d2d04f62390
Yugal Pariyar <yugalkhati570@gmail.com>
2022-05-06 12:02:25 +0545

    added build folder

--- src/components/mainQAComponent/AnswerComponent.tsx
+++ src/components/mainQAComponent/AnswerComponent.tsx
@@ -22,7 +22,6 @@ const AnswerComponent: FC<answerComponentInterface> = ({ question_id }) => {
   const [answerType, setAnswerType] = useState<string>("");
   const [showImageModal, setShowImageModal] = useState<boolean>(false);
 
-
   const handleAnswerSubmit:
     | React.MouseEventHandler<HTMLButtonElement>
     | undefined = () => {
